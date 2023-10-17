7e6b210464d972f59f24c1af2fe34bc880034ca1
Stanisław Mnich <stanislaw.mnich@student.uj.edu.pl>
2023-10-17 23:25:38 +0200

    2.18 and 2.19 add

---
+++ 2/2.18.py
@@ -0,0 +1,7 @@
+n = 123162749695698134758201892345781209302394
+
+liczba = str(n)
+
+liczba_zer = liczba.count('0')
+
+print(f"Liczba zer w liczbie {n} wynosi: {liczba_zer}")
---
+++ 2/2.19.py
@@ -0,0 +1,7 @@
+L = [7, 24, 60, 71, 101, 202, 300]
+
+bloki = [str(liczba).zfill(3) for liczba in L]
+
+wynik = ''.join(bloki)
+
+print(wynik)
