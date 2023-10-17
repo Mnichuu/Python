159174c60f5c69a4cc3eb19b39557bfcf66969d3
Stanisław Mnich <stanislaw.mnich@student.uj.edu.pl>
2023-10-17 17:15:51 +0200

    2.13 without sum()

--- 2/2.13.py
+++ 2/2.13.py
@@ -1,1 +1,5 @@
 line = "Patrzę na was, głodnych, przerażonych, przyciskających dzieci do pierśi"
+licznik = 0
+for wyraz in line.split():
+    licznik = licznik + 1
+print(f" Łączna ilośc wyrazów: {licznik} ")
