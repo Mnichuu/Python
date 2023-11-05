b64b15cf6c7da7ffdecf1532d3b3589712a59569
Stanisław Mnich <stanislaw.mnich@student.uj.edu.pl>
2023-11-05 16:53:32 +0100

    VISUAL UPDATE

--- 4/4.6.py
+++ 4/4.6.py
@@ -9,6 +9,7 @@ def sum_seq(sequence):
 
     return total_sum
 
+
 seq = [1, 2, [3, 4], (5, 6), [7, 8, [9, 10]]]
 wynik = sum_seq(seq)
 print(wynik)
--- 4/4.7.py
+++ 4/4.7.py
@@ -9,5 +9,6 @@ def flatten(sequence):
 
     return flattened
 
+
 seq = [1, (2, 3), [], [4, (5, 6, 7)], 8, [9]]
 print(flatten(seq))  # Wynik: [1, 2, 3, 4, 5, 6, 7, 8, 9]
