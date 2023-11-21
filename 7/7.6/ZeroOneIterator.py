a6714bee213226fdd58a168b2c6337388698c2ea
stanislaw <stanislaw.mnich@student.uj.edu.pl>
2023-11-21 13:09:37 +0100

    ADD 7.6 Zestaw 7

---
+++ 7/7.6/ZeroOneIterator.py
@@ -0,0 +1,18 @@
+class ZeroOneIterator:
+    value = 0
+
+    def __iter__(self):
+        self.value = 0
+        return self
+
+    def __next__(self):
+        result = self.value
+        self.value = 1 - self.value
+        return result
+
+
+print("====Iteratior0-1=====")
+zero_one_iter = ZeroOneIterator()
+for _ in range(10):
+    print(next(zero_one_iter))
+print("=====================")
