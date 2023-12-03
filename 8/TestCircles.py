d928f33dab2f3bb208619c9c317ae0478ff7ec8d
Stanisław Mnich <stanislaw.mnich@student.uj.edu.pl>
2023-12-03 19:54:20 +0100

    update

--- 8/TestCircles.py
+++ 8/TestCircles.py
@@ -1,45 +1,90 @@
-import unittest
 from circles import Circle
+from point import Point
+import pytest
 
 
-class TestCircle(unittest.TestCase):
-    def setUp(self):
-        self.circle1 = Circle(0, 0, 5)
-        self.circle2 = Circle(3, 4, 2)
-        self.circle3 = Circle(10, 5, 3)
-
-    def test_init(self):
-        with self.assertRaises(ValueError):
-            Circle('x', 2, 8)
-        with self.assertRaises(ValueError):
-            Circle(1, 3, -6)
-
-    def test_repr(self):
-        self.assertEqual(repr(self.circle1), "Circle(0, 0, 5)")
-
-    def test_eq(self):
-        self.assertEqual(self.circle1, Circle(0, 0, 5))
-        self.assertNotEqual(self.circle1, self.circle2)
-
-    def test_ne(self):
-        self.assertNotEqual(self.circle1, self.circle2)
-        self.assertNotEqual(self.circle1, self.circle3)
-
-    def test_area(self):
-        self.assertAlmostEqual(self.circle1.area(), 78.54, places=2)
-
-    def test_move(self):
-        self.circle1.move(2, 3)
-        self.assertEqual(repr(self.circle1), "Circle(2, 3, 5)")
-        with self.assertRaises(ValueError):
-            self.circle1.move('a', 3)
-        with self.assertRaises(ValueError):
-            self.circle2.move(3, '2')
-
-    def test_cover(self):
-        covered_circle = self.circle1.cover(self.circle2)
-        self.assertEqual(repr(covered_circle), "Circle(1.5, 2.0, 5.0)")
+def test_circle_top():
+    circle = Circle(0, 0, 5)
+    assert circle.top == 5
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_circle_left():
+    circle = Circle(0, 0, 5)
+    assert circle.left == -5
+
+
+def test_circle_bottom():
+    circle = Circle(0, 0, 5)
+    assert circle.bottom == -5
+
+
+def test_circle_right():
+    circle = Circle(0, 0, 5)
+    assert circle.right == 5
+
+
+def test_circle_width():
+    circle = Circle(0, 0, 5)
+    assert circle.width == 10
+
+
+def test_circle_height():
+    circle = Circle(0, 0, 5)
+    assert circle.height == 10
+
+
+def test_circle_topleft():
+    circle = Circle(0, 0, 5)
+    assert circle.topleft == Point(-5, 5)
+
+
+def test_circle_bottomleft():
+    circle = Circle(0, 0, 5)
+    assert circle.bottomleft == Point(-5, -5)
+
+
+def test_circle_topright():
+    circle = Circle(0, 0, 5)
+    assert circle.topright == Point(5, 5)
+
+
+def test_circle_bottomright():
+    circle = Circle(0, 0, 5)
+    assert circle.bottomright == Point(5, -5)
+
+
+def test_circle_creation():
+    circle = Circle(1, 2, 7)
+    assert circle.pt == Point(1, 2)
+    assert circle.radius == 7
+
+
+def test_circle_area():
+    circle = Circle(1, 2, 7)
+    assert circle.area() == pytest.approx(153.94, rel=1e-2)
+
+
+def test_circle_move():
+    circle = Circle(1, 2, 7)
+    circle.move(3, 4)
+    assert circle.pt == Point(4, 6)
+
+
+def test_circle_cover():
+    circle1 = Circle(0, 0, 5)
+    circle2 = Circle(0, 0, 3)
+    circle3 = Circle(10, 10, 13)
+
+    assert circle1.cover(circle2) == circle1
+    assert circle1.cover(circle3) == circle3
+    assert circle2.cover(circle3) == circle3
+
+
+def test_circle_from_points():
+    point1 = Point(0, 0)
+    point2 = Point(3, 0)
+    point3 = Point(0, 4)
+    circle = Circle.from_points((point1, point2, point3))
+
+    assert circle.pt == Point(1.5, 2)
+    assert circle.radius == pytest.approx(2.5, rel=1e-2)
--- 8/circles.py
+++ 8/circles.py
@@ -35,3 +35,71 @@ class Circle:
         center_x = (self.pt.x + other.pt.x) / 2
         center_y = (self.pt.y + other.pt.y) / 2
         return Circle(center_x, center_y, max_radius)
+
+    @classmethod
+    def from_points(cls, points):
+        if len(points) != 3 or not all(isinstance(p, Point) for p in points):
+            raise ValueError("from_points wymaga trzech punktów")
+
+        x1, y1 = points[0].x, points[0].y
+        x2, y2 = points[1].x, points[1].y
+        x3, y3 = points[2].x, points[2].y
+
+        a = x1 - x2
+        b = y1 - y2
+        c = x1 - x3
+        d = y1 - y3
+        e = (x1 ** 2 - x2 ** 2 + y1 ** 2 - y2 ** 2) / 2
+        f = (x1 ** 2 - x3 ** 2 + y1 ** 2 - y3 ** 2) / 2
+
+        det = b * c - a * d
+
+        if det == 0:
+            raise ValueError("Podane punkty są współliniowe")
+
+        x = (e * d - b * f) / det
+        y = (a * f - e * c) / det
+
+        radius = math.sqrt((x - x1) ** 2 + (y - y1) ** 2)
+
+        return cls(x, y, radius)
+
+    @property
+    def top(self):
+        return self.pt.y + self.radius
+
+    @property
+    def left(self):
+        return self.pt.x - self.radius
+
+    @property
+    def bottom(self):
+        return self.pt.y - self.radius
+
+    @property
+    def right(self):
+        return self.pt.x + self.radius
+
+    @property
+    def width(self):
+        return 2 * self.radius
+
+    @property
+    def height(self):
+        return 2 * self.radius
+
+    @property
+    def topleft(self):
+        return Point(self.left, self.top)
+
+    @property
+    def bottomleft(self):
+        return Point(self.left, self.bottom)
+
+    @property
+    def topright(self):
+        return Point(self.right, self.top)
+
+    @property
+    def bottomright(self):
+        return Point(self.right, self.bottom)
